3e5b1518411be36de6070fc2e5173a7547bea02d
Ashwin <ashwinramachandrang@gmail.com>
2021-05-09 16:10:17 +0530

    heroku push commit

--- routes/index.js
+++ routes/index.js
@@ -67,7 +67,7 @@ router.get('/python', (req, res) => {
 
   var dataToSend;
   // spawn new child process to call the python script
-  const python = spawn('python3', ['model.py', req.query.id]);
+  const python = spawn('python', ['model.py', req.query.id]);
   // collect data from script
   python.stdout.on('data', function (data) {
     console.log('Pipe data from python script ...');
